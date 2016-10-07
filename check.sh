fdc66a24d6841ee9318d83b8ea928d99edcdd5e5
redmaner <jakevdputten@gmail.com>
2016-10-07 14:05:57 +0200

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+check_basic () {
+		max_proces; xml_check_parser &
+		max_proces; xml_check_doubles &
+		max_proces; xml_check_apostrophe &
+		max_proces; xml_check_values &
+		max_proces; xml_check_plus &
+		max_proces; xml_check_variables &
+                max_proces; xml_check_formatted_false &
+} 
+
+check_normal () {
+		max_proces; xml_check_parser &
+		max_proces; xml_check_doubles &
+		max_proces; xml_check_apostrophe &
+		max_proces; xml_check_values &
+		max_proces; xml_check_plus &
+		max_proces; xml_check_variables &
+		max_proces; xml_check_formatted_false &
+                max_proces; xml_check_untranslateable &
+}       
+
+check_fix () {
+		max_proces; xml_check_parser &
+		max_proces; xml_check_doubles &
+		max_proces; xml_check_apostrophe &
+		max_proces; xml_check_values &
+		max_proces; xml_check_plus &
+		max_proces; xml_check_variables &
+		max_proces; xml_check_formatted_false &
+                max_proces; xml_fix_untranslateable &
+}
