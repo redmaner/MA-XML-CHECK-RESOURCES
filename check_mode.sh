1956db7db91900846894743dd2211b277730f42e
Redmaner <jakevdputten@gmail.com>
2016-10-08 16:49:06 +0200

    res: Add fix parameter in languages.xml

--- check_mode.sh
+++ check_mode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# modes for the check= parameter in languages.xml
 check_basic () {
 		max_proces; xml_check_parser &
 		max_proces; xml_check_doubles &
@@ -10,7 +11,7 @@ check_basic () {
         max_proces; xml_check_formatted_false &
 } 
 
-check_normal () {
+check_all () {
 		max_proces; xml_check_parser &
 		max_proces; xml_check_doubles &
 		max_proces; xml_check_apostrophe &
@@ -21,13 +22,7 @@ check_normal () {
         max_proces; xml_check_untranslateable &
 }       
 
-check_fix () {
-		max_proces; xml_check_parser &
-		max_proces; xml_check_doubles &
-		max_proces; xml_check_apostrophe &
-		max_proces; xml_check_values &
-		max_proces; xml_check_plus &
-		max_proces; xml_check_variables &
-		max_proces; xml_check_formatted_false &
+# modes for the fix= parameter in languages.xml
+fix_untranslateable () {
         max_proces; xml_fix_untranslateable &
 }
