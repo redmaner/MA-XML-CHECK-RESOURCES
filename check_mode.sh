73d97095766b16b7a99161b492daac371b4a26a9
Redmaner <jakevdputten@gmail.com>
2018-06-02 14:11:25 +0200

    Add double fix check mode
    Signed-off-by: Redmaner <jakevdputten@gmail.com>

--- check_mode.sh
+++ check_mode.sh
@@ -20,9 +20,24 @@ check_all () {
 		max_proces; xml_check_variables &
 		max_proces; xml_check_formatted_false &
         max_proces; xml_check_untranslateable &
+}     
+
+check_all_fix () {
+		max_proces; xml_check_parser &
+		max_proces; xml_check_apostrophe &
+		max_proces; xml_check_values &
+		max_proces; xml_check_plus &
+		max_proces; xml_check_variables &
+		max_proces; xml_check_formatted_false &
 }       
+  
 
 # modes for the fix= parameter in languages.xml
 fix_untranslateable () {
+        max_proces; xml_fix_untranslateable &
+}
+
+fix_all () {
+        max_proces; xml_fix_double &
         max_proces; xml_fix_untranslateable &
 }
