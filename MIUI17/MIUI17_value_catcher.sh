3dd9db656bf901e5d6cdfc4c679d3d4735abf02e
ingbrzy <ingbrzy@miuios.cz>
2025-11-06 16:44:28 +0100

    HOS3

---
+++ MIUI17/MIUI17_value_catcher.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+catch_values_arrays () {
+cat << EOF
+alphabet_chars
+BacklightValues
+color_mode_ids
+default
+entryValues
+entryvalues
+prefValues
+Value
+value
+values
+VibePattern
+vibration_torch
+EOF
+}
+
+catch_values_strings () {
+cat << EOF
+entryvalue
+default
+entryValues
+prefValues
+Value
+value
+values
+EOF
+}
